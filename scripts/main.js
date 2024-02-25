ef3aae3495ef135c6f19f5f9c96e87c0b9562b5b
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-02-25 16:49:35 -0300

    feat: excluir posts

--- scripts/main.js
+++ scripts/main.js
@@ -6,29 +6,32 @@ const publishPost = () => {
   let content = post.value.trim();
 
   if (content !== "") {
+    const uuid = self.crypto.randomUUID();
+    console.log(uuid);
     const currentDate = new Date();
     const currentHour = currentDate.getHours();
     const currentMin = currentDate.getMinutes();
     const timestamp = `${currentDate.toLocaleDateString()} ${currentHour}:${currentMin}`;
     const newPost = `
-        <div class="card">
+        <div id="${uuid}" class="card">
             <p class="data">${timestamp}</p>
             <div class="content">${content}</div>
+            <button onclick="deletePost('${uuid}')">Deletar</button>
         </div>
         `;
     feed.innerHTML = newPost + feed.innerHTML;
 
-    savePost(timestamp, content);
+    savePost(timestamp, content, uuid);
 
     post.value = "";
   }
 };
 
 //função para salvar post no localStorage
-const savePost = (timestamp, content) => {
+const savePost = (timestamp, content, uuid) => {
   let posts = JSON.parse(localStorage.getItem("posts")) || [];
 
-  posts.push({ timestamp, content });
+  posts.push({ timestamp, content, uuid });
 
   localStorage.setItem("posts", JSON.stringify(posts));
 };
@@ -42,15 +45,25 @@ const loadPosts = () => {
   let postsHTML = posts
     .map(
       (post) => `
-    <div class="card">
+    <div id="${post.uuid}" class="card">
     <p class="data">${post.timestamp}</p>
     <div class="content">${post.content}</div>
+    <button onclick="deletePost('${post.uuid}')">Deletar</button>
 </div>
     `
     )
     .join("");
 
   feed.innerHTML = postsHTML;
+};
+
+//função para deletar um post
+const deletePost = (uuid) => {
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+  posts = posts.filter((post) => post.uuid !== uuid);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  loadPosts();
 };
 
 loadPosts();
